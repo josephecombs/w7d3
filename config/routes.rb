395ab97dc00ec7ad6a84ebf504aac9293aaebe4e
Joseph Combs <joseph.e.combs@gmail.com>
2014-09-17 17:42:20 -0700

    Through Phase 8 (basically)

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 NewAuthDemo::Application.routes.draw do
   resources :users, :only => [:create, :new, :show]
-  resource :session, :only => [:create, :destroy, :new]
+  resource :session, :only => [:create, :destroy]
 
   get '/contact/', to: 'staticpages#contact', as: 'contact'
   get '/home/', to: 'staticpages#home', as: 'home'
   get '/about/', to: 'staticpages#about', as: 'about'
 
-  root :to => "users#show"
+  root :to => "staticpages#home"
 end
